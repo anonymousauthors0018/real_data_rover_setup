12e86c076b9f5d27fe2ae0693f9acf9599680240
Anonymous Authors <anonymousauthors0018@gmail.com>
2026-06-04 15:36:32 -0400

    robot control code

---
+++ wheeltec/src/qt_ros_test/script/qt_ros_test.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+### BEGIN INIT
+
+gnome-terminal -- bash -c "source /opt/ros/melodic/setup.bash;source /home/wheeltec/wheeltec_robot/devel/setup.bash;roscore"
+sleep 5
+gnome-terminal -- bash -c "source /opt/ros/melodic/setup.bash;source /home/wheeltec/wheeltec_robot/devel/setup.bash;rosrun qt_ros_test qt_ros_test"
+
+wait
+exit 0
+
+
